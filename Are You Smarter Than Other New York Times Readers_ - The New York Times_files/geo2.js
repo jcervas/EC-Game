155e2ac60cdaf75d6efc3bac03165a244997a549
Jonathan Cervas <jcervas@uci.edu>
2020-03-13 10:49:41 -0700

    NYT for Reference

---
+++ Are You Smarter Than Other New York Times Readers_ - The New York Times_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "IRVINE",
+      'continent': "NA",
+      'country': "US",
+      'region': "CA"
+    },
+    'ip':"169.234.205.244"
+  }]);
+})
+//
+()
+
+;
